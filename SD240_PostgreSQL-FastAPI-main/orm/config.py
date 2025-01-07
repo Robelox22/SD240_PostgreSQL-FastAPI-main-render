c772e119183e6efe21e2b205a30c0919f4185ad4
Robelox22 <robellfernando@gmail.com>
2025-01-07 13:15:23 -0600

    segundo commit

--- SD240_PostgreSQL-FastAPI-main/orm/config.py
+++ SD240_PostgreSQL-FastAPI-main/orm/config.py
@@ -5,16 +5,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 # declarative_base permite definir la clase base para mapear las tablas de la BD
 from sqlalchemy.ext.declarative import declarative_base
-
+#importar el archivo de modelos
+from orm import modelos
+import os
 #1. Configurar la conexion BD
 # Crear la URL de la BD -> servidorBD://usuario:password@url:puerto/nombreBD
-URL_BASE_DATOS = "postgresql://usuario-ejemplo:12345@localhost:5432/base-ejemplo"
+#URL_BASE_DATOS = "postgresql://usuario-ejemplo:12345@localhost:5432/base-ejemplo"
 # Conectarnos mediante el esquema app
-engine = create_engine(URL_BASE_DATOS,
-                       connect_args={
-                           "options": "-csearch_path=app"                           
-                       })
-
+#engine = create_engine(URL_BASE_DATOS,
+ #                      connect_args={
+  #                         "options": "-csearch_path=app"                           
+   #                    })
+engine = create_engine(os.getenv("db_uri","sqlite://base-ejemplo.db"))
 #2. Obtener la clase que nos permite crear objetos tipo session
 SessionClass = sessionmaker(engine) 
 # Crear una función para obtener objetos de la clase SessionClass
